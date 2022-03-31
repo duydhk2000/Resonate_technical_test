413a58b6d3eb2752430aea4ddec0fc3f2ec1ee8a
Duy Do <hdoo0010@student.monash.edu>
2022-03-31 14:39:35 +1100

    Add Gallery

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import Gallery from './components/Gallery'
 
 function App() {
   return (
     <div className="App">
         <NavBar />
         <div className={`App-content`} style={{justifyContent: "center", alignItems: "center"}}>
-
+            <Gallery></Gallery>
         </div>
     </div>
   );
