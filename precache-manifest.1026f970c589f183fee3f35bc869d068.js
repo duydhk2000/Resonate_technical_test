ad8e35a06b19fdf38546fefda1ac340778b4979d
Duy Do <hdoo0010@student.monash.edu>
2022-03-31 23:37:46 +1100

    Updates

---
+++ precache-manifest.1026f970c589f183fee3f35bc869d068.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "96b4f3d59c02f756e7b0",
+    "url": "/react-gh-pages/static/css/main.de251eb0.chunk.css"
+  },
+  {
+    "revision": "96b4f3d59c02f756e7b0",
+    "url": "/react-gh-pages/static/js/main.96b4f3d5.chunk.js"
+  },
+  {
+    "revision": "6d1c687f73f1a0446a07",
+    "url": "/react-gh-pages/static/js/1.6d1c687f.chunk.js"
+  },
+  {
+    "revision": "b9f507903b730ee2a96c",
+    "url": "/react-gh-pages/static/js/2.b9f50790.chunk.js"
+  },
+  {
+    "revision": "88a9303dcb302ffd3a5f",
+    "url": "/react-gh-pages/static/js/runtime~main.88a9303d.js"
+  },
+  {
+    "revision": "3ed865684607ea4ee1d32ba569ea39c3",
+    "url": "/react-gh-pages/index.html"
+  }
+];
